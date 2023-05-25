8d8a03cd3e0d8117f872087c8a435fd7e68c3fa9
김인범 <dlsqja970210@naver.com>
2023-05-26 07:18:20 +0900

    dsd

--- ssafit_front/argon/src/components/article/Free.vue
+++ ssafit_front/argon/src/components/article/Free.vue
@@ -3,45 +3,36 @@
     <h2>자유게시판</h2>
     <h4>등록된 사용자의 수 : {{ articleCnt }}</h4>
     <div v-if="articleCnt">
-      <table class="user-list">
-        <colgroup>
-          <col style="width: 10%" />
-          <col style="width: 25%" />
-          <col style="width: 15%" />
-          <col style="width: 10%" />
-          <col style="width: 10%" />
-          <col style="width: 10%" />
-          <col style="width: 30%" />
-        </colgroup>
-        <thead head-variant="dark">
-          <tr>
-            <th>번호</th>
-            <th>제목</th>
-            <th>작성자</th>
-            <th>좋아요</th>
-            <th>싫어요</th>
-            <th>조회수</th>
-            <th>등록날짜</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(article, index) in articles" :key="index">
-            <td>{{ index + 1 }}</td>
-            <td>
+      <b-table-simple hover :items="articles" class="user-list">
+        <b-thead class="text-center">
+          <b-tr>
+            <b-th>번호</b-th>
+            <b-th>제목</b-th>
+            <b-th>작성자</b-th>
+            <b-th>좋아요</b-th>
+            <b-th>싫어요</b-th>
+            <b-th>조회수</b-th>
+            <b-th>등록날짜</b-th>
+          </b-tr>
+        </b-thead>
+        <b-tbody>
+          <b-tr v-for="(article, index) in articles" :key="index">
+            <b-td>{{ index + 1 }}</b-td>
+            <b-td>
               <router-link
-                class="article-link"
+                b-class="article-link"
                 :to="`detail/${article.article_id}`"
                 >{{ article.article_title }}</router-link
               >
-            </td>
-            <td>{{ article.user_id }}</td>
-            <td>{{ article.article_like_cnt }} 개</td>
-            <td>{{ article.article_unlike_cnt }} 개</td>
-            <td>{{ article.article_view_cnt }} 번</td>
-            <td>{{ article.article_reg_date }}</td>
-          </tr>
-        </tbody>
-      </table>
+            </b-td>
+            <b-td>{{ article.user_id }}</b-td>
+            <b-td>{{ article.article_like_cnt }} 개</b-td>
+            <b-td>{{ article.article_unlike_cnt }} 개</b-td>
+            <b-td>{{ article.article_view_cnt }} 번</b-td>
+            <b-td>{{ article.article_reg_date }}</b-td>
+          </b-tr>
+        </b-tbody>
+      </b-table-simple>
     </div>
     <div v-else>등록된 글이 없습니다.</div>
   </div>
